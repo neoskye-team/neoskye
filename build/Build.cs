0e1a7023f9ea3c38aa1f339aa299322e2d202525
zTags <wierdgeomery@gmail.com>
2023-03-01 20:40:16 +0100

    finish engine api draft

--- build/Build.cs
+++ build/Build.cs
@@ -1,5 +1,7 @@
 using System;
+using System.IO;
 using System.Linq;
+using System.Diagnostics;
 
 using Nuke.Common;
 using Nuke.Common.CI;
@@ -12,6 +14,9 @@ using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
+
+// TODO: move the test build to somewhere else
 
 class Build : NukeBuild {
   /// Support plugins are available for:
@@ -25,6 +30,8 @@ class Build : NukeBuild {
   [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
   readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+  [Solution] Solution Solution;
+
   Target Clean => _ => _
       .Before(Restore)
       .Executes(() => {
@@ -32,10 +39,23 @@ class Build : NukeBuild {
 
   Target Restore => _ => _
       .Executes(() => {
+        MSBuild($"{Solution} /target:Restore /p:Configuration={Configuration} /nr:false");
+        if (IsLocalBuild)
+          MSBuild($"{Directory.GetCurrentDirectory()}/test/test.csproj /target:Restore /p:Configuration={Configuration} /nr:false");
       });
 
   Target Compile => _ => _
       .DependsOn(Restore)
       .Executes(() => {
+        MSBuild($"{Solution} /target:Build /p:Configuration={Configuration} /nr:false");
+        if (IsLocalBuild)
+          MSBuild($"{Directory.GetCurrentDirectory()}/test/test.csproj /target:Build /p:Configuration={Configuration} /nr:false");
       });
+
+  Target RunTest => _ => _
+    .DependsOn(Compile)
+    .Executes(() => {
+      var p = Process.Start($"{Directory.GetCurrentDirectory()}/test/bin/Debug/net7.0/test.exe");
+      p.WaitForExit();
+    });
 }
